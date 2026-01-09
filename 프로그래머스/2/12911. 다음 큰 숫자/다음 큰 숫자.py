7056cd2d0de9aaec9b730b92e1f6ef3c957b1526
jjwoori123-lang <jjwoori123@gmail.com>
2026-01-09 16:51:10 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,5 +1,5 @@
 def solution(n):
     cnt= str(bin(n)).count("1")
-    for i in range(n+1, 1000001):
+    for i in range(n+1, 1000065):
         if str(bin(i)).count("1") == cnt:
             return i
