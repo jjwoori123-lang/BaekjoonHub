152c2a83d2eaa2dbb301ef668ecf64bada229e28
jjwoori123-lang <jjwoori123@gmail.com>
2026-01-10 12:12:41 +0900

    [Silver V] Title: 집합, Time: 972 ms, Memory: 128708 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,28 +1,27 @@
 import sys
 input = sys.stdin.readline
-set1 = set(list())
-for _ in range(int(input())):
-    list_a = list(input().split())
-    if len(list_a) == 2:
-        a,b = list_a
-        if a == "add":
-            set1.add(int(b))
-        if a == 'check':
-            if int(b) in set1:
+n = int(input())
+start = 0
+for _ in range(n):
+    cal = list(input().split())
+    if len(cal) == 1:
+        if cal[0] == 'all':
+            start =  (1 << 20) - 1
+            start = int(start)
+        else:
+            start = 0
+    else:
+        a,b = cal
+        b = int(b)
+        if cal[0] == 'add':
+            start |= 1<<(b-1)
+        elif cal[0] == 'remove':
+            start &= ~( 1 << (b-1))
+        elif cal[0] == 'check':
+            chk  = start & ( 1 << (b-1))
+            if chk: 
                 print(1)
             else:
                 print(0)
-        if a == 'remove':
-            if int(b) in set1:
-                set1.discard(int(b))
-        if a == 'toggle':
-            if int(b) in set1:
-                set1.discard(int(b))
-            else:
-                set1.add(int(b))
-    elif len(list_a) == 1:
-        if list_a[0] == "all":
-            set1 = set([i for i in range(1,21)])
-        else:
-            set1 = set(list())
-        
+        elif cal[0] == 'toggle':
+            start ^= ( 1 << (b-1))
