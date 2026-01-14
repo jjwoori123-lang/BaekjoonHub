c6c63cbcc59547c033295d4cf028b9c6cdf21e4e
jjwoori123-lang <jjwoori123@gmail.com>
2026-01-14 15:24:00 +0900

    [Gold V] Title: 치킨 배달, Time: 176 ms, Memory: 111956 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,31 +1,30 @@
 n,m = map(int, input().split())
 
-def get_dist(chick):
-    total_dist = 0
+def get_dist(s):
+    tot_dist = 0
 
     for x,y in city:
-        min_df = float('inf')  
-        for cx, cy in chick:
-            dist = abs(x-cx) + abs(y-cy)
-            min_df = min(min_df, dist)
-        total_dist += min_df
-    return total_dist
-
+        min_df = float('inf')
+        for r,c in s:
+            min_df = min(min_df, abs(r-x) + abs(y-c))
+        tot_dist+= min_df
+    return tot_dist
 
 def dfs(idx, cnt):
     global ans
-
     if cnt == m:
-        selected = [chick[i] for i in range(len(chick)) if visit[i]]
-        ans = min(ans, get_dist(selected))
+        select = [chick[i] for i in range(len(chick)) if visit[i]]
+        ans = min(ans, get_dist(select))
         return
-    if idx == len(chick):
-        return
+    
+    if idx == len(chick): return
+    
     visit[idx] = True
     dfs(idx+1, cnt+1)
 
     visit[idx] = False
     dfs(idx+1, cnt)
+
 
 arr = [list(map(int, input().split())) for _ in range(n)]
 chick = list()
