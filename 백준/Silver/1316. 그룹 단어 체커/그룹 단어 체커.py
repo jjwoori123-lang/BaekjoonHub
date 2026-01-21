3829472637aa85aac5fc857fb8545d54cb965a1d
jjwoori123-lang <jjwoori123@gmail.com>
2026-01-21 10:13:55 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 76 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -2,17 +2,11 @@ n = int(input())
 res = 0
 for _ in range(n):
     chk = True
-    stack = []
     s = list(input())
-    stack.append(s[0])
+    stack = [s[0]]
     for j in range(1, len(s)):
-        if stack[-1] == s[j]:
-            pass
-        else:
-            if s[j] in stack:
-                chk = False
-            else:
-                stack.append(s[j])
-    if chk:
-        res+=1
+        if stack[-1] != s[j]:
+            if s[j] in stack: chk = False
+            else: stack.append(s[j])
+    if chk: res+=1
 print(res)
