91bee10fd74999a7835dc5c86e58c42661c49778
jjwoori123-lang <jjwoori123@gmail.com>
2026-02-10 17:15:15 +0900

    [Gold V] Title: 최소비용 구하기, Time: 284 ms, Memory: 122948 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,26 @@
+import heapq
+
+def dijkstr(s,e):
+    q = []
+    heapq.heappush(q, (0, s))
+    while q:
+        cost, start = heapq.heappop(q)
+        if cost > visit[start]: continue
+        for nxt, ncost in graph[start]:
+            ndist = ncost + cost
+            if visit[nxt] > ndist:
+                visit[nxt] = ndist
+                heapq.heappush(q, (ndist, nxt))
+
+n = int(input())
+m = int(input())
+
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    a,b,c = map(int, input().split())
+    graph[a].append((b,c))
+s,e = map(int, input().split())
+visit = [int(1e9)] * (n+1)
+visit[s] = 0
+dijkstr(s,e)
+print(visit[e])
