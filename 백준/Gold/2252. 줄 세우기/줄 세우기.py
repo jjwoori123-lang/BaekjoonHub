ca3ab5dadf272065f4e0167d464faed94590d033
jjwoori123-lang <jjwoori123@gmail.com>
2026-02-25 17:24:52 +0900

    [Gold III] Title: 줄 세우기, Time: 224 ms, Memory: 117904 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+n, m = map(int, input().split())
+indegree = [0] * (n+1)
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a,b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b]+=1
+
+def topology():
+    res = []
+    q = deque()
+    for i in range(1, n+1):
+        if not indegree[i]: q.append(i)
+    while q:
+        x = q.popleft()
+        res.append(x)
+        for nx in graph[x]:
+            indegree[nx]-=1
+            if not indegree[nx]:q.append(nx)
+    for r in res: print(r, end = " ")
+topology()
