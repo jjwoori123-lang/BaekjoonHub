d7234d7ab80ddc38a12a57a68f1fbdba5c6078cf
jjwoori123-lang <jjwoori123@gmail.com>
2026-03-24 12:11:17 +0900

    [Gold IV] Title: 사냥꾼, Time: 224 ms, Memory: 132184 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -4,11 +4,13 @@ def cal_dist(x,x1,y1):
     return abs(x-x1) + y1
 n,m,l = map(int, input().split())
 gun = list(map(int, input().split()))
+gun.sort()
 deer = []
 for _ in range(m):deer.append(list(map(int, input().split())))
 cnt = 0
 for i, j in deer:
     a,b = 0, len(gun)-1
+    if j>l: continue
     while a<=b:
         mid = (a+b)//2
         if gun[mid] > i + l - j:
